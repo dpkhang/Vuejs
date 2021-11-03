cc5ecc08dc55d99203b841bdf344021b509606d4
dpkhang <khangphuc1@gmail.com>
2021-11-04 02:55:47 +0700

    first commit

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="title">
+        <h1>First Project</h1>
+        {{title}}
+    </div>
+</template>
+
+<script>
+    import './Home.scss'
+    export default {
+        name: 'home',
+        data () {
+            return {
+                title: 'Hello World',
+            }
+        }
+    }
+</script>
